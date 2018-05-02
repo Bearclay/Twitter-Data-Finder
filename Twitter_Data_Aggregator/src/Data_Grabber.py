4876e5eb30bdd9520852d4a6d7fda8d15395819e
Bearclay <Rgbarc@gmail.com>
2018-05-01 20:14:35 -0400

    Code that pulls Tweets from Twitter API, then saves them too tweets.json. This is the first commit for the final project.

---
+++ Twitter_Data_Aggregator/src/Data_Grabber.py
@@ -0,0 +1,40 @@
+import json
+from tweepy.streaming import StreamListener
+from tweepy import Stream
+import tweepy
+
+
+
+with open("twitter_credentials.json", "r") as file:
+    creds = json.load(file)
+
+
+    
+class FileWriteListener(StreamListener):
+    
+    # Returns a "FileWriteListener" object
+    def __init__(self):
+        # Refers to "FileWriteListener" class implicitly
+        super().__init__()
+        self.save_file = open('tweets.json','a')
+        self.tweets = []
+
+    def on_data(self, tweet):
+        self.tweets.append(json.loads(tweet))
+        self.save_file.write(str(tweet))
+
+    def on_error(self, status):
+        print(status)
+        return True
+    
+      
+if __name__ == '__main__':
+    listener = FileWriteListener()
+    auth = tweepy.OAuthHandler(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+    auth.set_access_token(creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
+      
+   
+    stream = Stream(auth, listener)
+    stream.filter(track=['programming'])
+    
+
